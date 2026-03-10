9860b07bfe8606f29a612c5e74d5cc051837563e
Zizou10 <paolo.pilati93@gmail.com>
2026-03-10 16:24:27 +0100

    Installazione Router e import in main.jsx e App + indicazione dei path URL per pagine Home, Chi siamo, Prodotti

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
 
   return (
     <>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/chisiamo" element={<ChiSiamo />} />
+        <Route path="/prodotti" element={<Prodotti />} />
+
+      </Routes>
 
     </>
   )
