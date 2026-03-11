07e61d3db746229bee485e8d064f909e5661ac04
Zizou10 <paolo.pilati93@gmail.com>
2026-03-11 17:51:59 +0100

    Stile pagina articolo (la card non è più renderizzata con il componente ProductCard, bensì con uno stile apposito per il rendering della pagina + gif per colorare la home.

--- my-react-app/src/pages/Articolo.jsx
+++ my-react-app/src/pages/Articolo.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import ProductCard from "../components/ProductCard";
 
 export default function Articolo () {
   const { id } = useParams();
@@ -17,11 +16,29 @@ export default function Articolo () {
 
   return (
     <div className="container mt-4">
-      <h1 className="mb-4">{product.title} - {id}</h1>
-
-      <div className="row g-3">
-        <div className="col-12">
-          <ProductCard product={product} />
+      <h1 className="mb-4">Articolo: {product.title} - {id}</h1>
+      <div className="card">
+        <div className="row g-0 align-items-center">
+          <div className="col-md-4">
+            <img
+              src={product.image}
+              className="img-fluid p-5"
+              alt={product.title}
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title mb-3">
+                ${product.price}
+              </h3>
+              <p className="card-text">
+                {product.description}
+              </p>
+              <p className="card-text text-muted">
+                Categoria: {product.category}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
     <div className="container mt-4">
-      <h1>Homepage</h1>
-      <p>Benvenuto nel nostro shop</p>
+      <div className="row">
+        <div className="col-12 d-flex flex-column align-items-center text-center">
+
+          <h1>Homepage</h1>
+          <p>Benvenuto nel nostro shop!!!</p>
+          <img
+            src="https://i.pinimg.com/originals/a9/ee/35/a9ee351d275f9b453c0535070ad5ff92.gif"
+            alt="Gif divertente da Adventure Times"
+            className="img-fluid rounded p-5"
+          />
+        </div>
+      </div>
     </div>
   )
 }
