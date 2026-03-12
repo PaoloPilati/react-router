a520c8849547c3d360b8c7ce58095f6737b5e499
Zizou10 <paolo.pilati93@gmail.com>
2026-03-12 15:07:32 +0100

    Bottoni (next-prev) posizionati fuori dalla card.

--- my-react-app/src/pages/Articolo.jsx
+++ my-react-app/src/pages/Articolo.jsx
@@ -18,6 +18,8 @@ export default function Articolo () {
   return (
     <div className="container my-5">
       <h1 className="mb-4">Articolo: {product.title} - {id}</h1>
+
+      {/* CARD */}
       <div className="card">
         <div className="row g-0 align-items-center">
           <div className="col-md-4">
@@ -29,30 +31,25 @@ export default function Articolo () {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h3 className="card-title mb-3">
-                ${product.price}
-              </h3>
-              <p className="card-text">
-                {product.description}
-              </p>
-              <p className="card-text text-muted">
-                Categoria: {product.category}
-              </p>
-            </div>            
-            <div className="row mt-3">
-              <div className="col d-flex justify-content-center gap-3">
-
-                <button className="btn btn-secondary">
-                  Precedente
-                </button>
-
-                <button className="btn btn-secondary">
-                  Successivo
-                </button>
-              </div>
+              <h3 className="card-title mb-3">${product.price}</h3>
+              <p className="card-text">{product.description}</p>
+              <p className="card-text text-muted">Categoria: {product.category}</p>
             </div>
           </div> 
         </div> 
+      </div>
+
+      {/* BOTTONI FUORI DALLA CARD */}
+      <div className="row mt-3">
+        <div className="col d-flex justify-content-center gap-3">
+          <button className="btn btn-secondary">
+            Precedente
+          </button>
+
+          <button className="btn btn-secondary">
+            Successivo
+          </button>
+        </div>
       </div>
     </div>
   )
